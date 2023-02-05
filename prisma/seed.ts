b164d36f4aa05195436f769baa3a07d6e40a6c23
mk422756 <k422756+git@gmail.com>
2023-02-05 18:58:05 +0900

    DBのseed追加

---
+++ prisma/seed.ts
@@ -0,0 +1,43 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+async function main() {
+  const alice = await prisma.user.upsert({
+    where: { email: "alice@prisma.io" },
+    update: {},
+    create: {
+      email: "alice@prisma.io",
+      name: "Alice",
+      tweets: {
+        create: {
+          text: "Check out Prisma with Next.js",
+        },
+      },
+    },
+  });
+  const bob = await prisma.user.upsert({
+    where: { email: "bob@prisma.io" },
+    update: {},
+    create: {
+      email: "bob@prisma.io",
+      name: "Bob",
+      tweets: {
+        create: [
+          {
+            text: "Follow Prisma on Twitter",
+          },
+        ],
+      },
+    },
+  });
+  console.log({ alice, bob });
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
